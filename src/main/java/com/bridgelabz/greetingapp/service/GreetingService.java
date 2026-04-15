e32fbb13afe3eb6988ca48d8ccb003334698de03
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-15 16:34:15 +0530

    [Jason] feat : add repository and save greeting

--- src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -1,11 +1,21 @@
 package com.bridgelabz.greetingapp.service;
 
+import com.bridgelabz.greetingapp.model.Greeting;
+import com.bridgelabz.greetingapp.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
 
+    @Autowired
+    private GreetingRepository repo;
+
     public String getSimpleGreeting() {
         return "Hello World";
+    }
+
+    public Greeting saveGreeting(String message) {
+        return repo.save(new Greeting(message));
     }
 }
