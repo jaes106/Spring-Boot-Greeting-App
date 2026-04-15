92c5abb3eeb1d656e1b11e21aab88751e41edf49
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-15 16:23:12 +0530

    [Jason] feat : add service layer for greeting

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,15 +1,18 @@
 package com.bridgelabz.greetingapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    @Autowired
+    private GreetingService service;
+
     @GetMapping
     public String getGreeting() {
-        return "Hello World";
+        return service.getSimpleGreeting();
     }
 }
