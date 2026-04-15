620ea4ff69a1546dd22c4ad7c4a79f237fb570c9
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-15 15:25:12 +0530

    [Jason] feat : add uc1 greeting controller basic json response

--- src/main/java/com/bridgelabz/greetingapp/GreetingAppApplication.java
+++ src/main/java/com/bridgelabz/greetingapp/GreetingAppApplication.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.GreetingApp;
+package com.bridgelabz.greetingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/bridgelabz/greetingapp/GreetingAppApplicationTests.java
+++ src/test/java/com/bridgelabz/greetingapp/GreetingAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.GreetingApp;
+package com.bridgelabz.greetingapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
